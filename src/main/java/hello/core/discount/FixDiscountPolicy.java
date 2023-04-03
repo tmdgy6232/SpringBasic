a340f5dece65033b39a5df9454877bf222f25903
seunghyo <tmdgy6232@naver.com>
2023-04-03 16:50:25 +0900

    create: order domain testcode

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -6,7 +6,7 @@ import hello.core.member.Member;
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000; // 1000원 할인
-    
+
     @Override
     public int discount(Member member, int price) {
         // enum클래스는 == 비교가 맞다.
