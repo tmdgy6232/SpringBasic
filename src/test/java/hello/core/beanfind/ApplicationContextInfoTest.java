0901b6c3d408376581936dc42924ca641ba0c48d
seunghyo <tmdgy6232@naver.com>
2023-04-13 20:59:39 +0900

    create : bean 조회하기

---
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,23 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+
+        for(String beanDefinitionName : beanDefinitionNames){
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("bean = " + beanDefinitionName + "obejct  = " + bean);
+        }
+    }
+}
