9631db94e0f7a2b39f076758d8b7e08970c08774
seunghyo <tmdgy6232@naver.com>
2023-05-15 16:19:01 +0900

    create : 싱글톤과 바이트코드 조작 feat  : CGLIB

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -14,23 +14,34 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    // @Configuration이 없으면
+    // class가 CGLIB기술을 쓰지 않고 그냥 생 클래스로 등록되기 때문에
+    // 설정에 관환 싱글톤을 보장해주지 않는다.
+    //@Bean memberService -> new MemorMemberRepository()
+    //@Bean orderService -> new MemorymemberREpository() 싱글톤이 깨지지 않는가
+
     @Bean
     public MemberService memberService(){
+        System.out.println("AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
-    public MemoryMemberRepository memberRepository() {
+    public MemoryMemberRepository memberRepository()
+    {
+        System.out.println("AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
     public DiscountPolicy discountPolicy(){
+        System.out.println("AppConfig.discountPolicy");\
         return new RateDiscountPolicy();
     }
 }
