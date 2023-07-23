b64990c16cf08beaf345e858b08d83e74dd6984b
seunghyo <tmdgy6232@naver.com>
2023-07-23 23:06:38 +0900

    bean이 중복되었을 때 빈을 주입하는 방법들

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,6 +4,7 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,7 +13,8 @@ public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy;
+    @Qualifier("mainDiscountPolicy")
+    private final DiscountPolicy rateDiscountPolicy;
 
     // 생성자가 한개만 있으면 @Autowired를 굳이 지정해주지 않아도 된다.
 
@@ -30,11 +32,29 @@ public class OrderServiceImpl implements OrderService {
 //        this.memberRepository = memberRepository;
 //        this.discountPolicy = discountPolicy;
 //    }
-
+/*
+*  빈이 겹칠 때에 @Autowired를 사용한다면
+*  1. Autowired
+*  타입으로 처음에 조회하고 두개일 시 변수명으로 매칭한다. 그래서 rateDiscounPolicy라고 해주니까
+*  잘 동작하는걿 확인할 수 있따.
+*
+*  2. @Quilifier 를 사용
+*  @Quilifier 추가 구분자를 붕텨주는 방법이다. 주입 시 추가적인 방법을 제공하는 것이지, 빈 이름을 변경하는것은 아니다.
+*  @Component 밑에 @Quilifier("aaa")로 지정하고 불러오는 부분에서 똑같이 사용한다.
+*  또한 이 이후에 찾지 못하면 1번과 같이 변수명으로도 매칭하는 동작을 이어 수행한다.*
+*  단점은 모든 코드에 @Quilifier를 붙여야한다. 하지만 primary는 안붙여줘도 되긴 한다.
+*
+* 3. @Primary 사용
+*  우선순위를 지정하는 방법이다. 같은 타입이 있으면 이 객체가 우선순위가 되어 주입된다.
+*
+*  위에 2번 3번을 동시에 사용하였을 때에는
+*  스프링은 자동보다는 수동이, 넓은범위의 선택권보다는 좁은 범위의 선택권이 우선순위가 높기 때문에
+*  Primary보다 Quilifier가 우선순위가 높다.
+* */
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
-        int discountPrice = discountPolicy.discount(member, itemPrice);
+        int discountPrice = rateDiscountPolicy.discount(member, itemPrice);
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 
