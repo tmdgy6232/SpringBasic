a34e75668a94f0c8dfc39274615686c4a81f1951
seunghyo <tmdgy6232@naver.com>
2023-10-09 22:10:58 +0900

    create: bean 콜백 생명주기, 생성, 소멸 인터페이스 사용

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,46 @@
+package hello.core.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+// 1. 인터페이스를 사용하여 초기화, 소멸 메서드 사용하는 방법.
+public class NetworkClient implements InitializingBean, DisposableBean {
+
+    private String url;
+
+    public NetworkClient(){
+        System.out.println("생성자 호출, url = " + url);
+    }
+
+    public void setUrl(String url){
+        this.url = url;
+    }
+
+    //서비스 시작 시 호출
+    public void connect(){
+        System.out.println("connect = " + url);
+    }
+
+    public void call(String message){
+        System.out.println("call = " + url + " message = " + message);
+    }
+
+    // 서비스 종료시 호출
+    public void disconnect(){
+        System.out.println("close = " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // 의존관계 주입이 끝나면 호출
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
+}
