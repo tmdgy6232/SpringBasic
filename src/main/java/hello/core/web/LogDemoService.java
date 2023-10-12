f6cba5d6995d43aaa2f0908ffc1a566a596508cf
seunghyo <tmdgy6232@naver.com>
2023-10-12 22:56:05 +0900

    create : Request bean scope provider 사용하여 올려보기

---
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -0,0 +1,16 @@
+package hello.core.web;
+
+import hello.core.discount.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+    public void logic(String testId) {
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.log("service id = " + testId);
+    }
+}
